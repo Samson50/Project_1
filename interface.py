840c8514e010a572b94caf794f016ed5f395e221
jamesbthomas <james.b.thomas@mac.com>
2017-05-04 22:50:20 -0400

    initial gui

--- interface.py
+++ interface.py
@@ -1,132 +1,114 @@
 import tkinter
+from tkinter import ttk
 
-class interface (tkinter.Tk):
-    def __init__(self,parent):
-        tkinter.Tk.__init__(self,parent)
-        self.parent = parent
-        self.initialize()
+class RootDNS:
+    def __init__(self):
+        self.entries = []
+        #self.entries.append(DNSEntry("tempName","tempType","tempVal"))
+        #self.entries.append(DNSEntry("tempName2","tempType2","tempVal2"))
+        
+    def addEntry(self,name,type,value,parent):
+        self.entries.append(DNSEntry(name,type,value))
+        updateDisplayFrame(parent,self.entries)
+
+class DNSEntry:
+    def __init__(self,name,type,value):
+        self.name = name
+        self.type = type
+        self.value = value
+        
+class Network:
+    def __init__(self):
+        self.entries = []
+        #self.entries.append("temp")
+        #self.entries.append("temp2")
      
-    def initialize(self):
-        self.grid()
-        
-        self.startButton = tkinter.Button(self, text="BEGIN",command=self.addInterfaces)
-        self.startButton.grid(column=0,row=0,sticky="EW",padx=5,pady=5)
-        
-        self.grid_columnconfigure(0,weight=1)
-        self.resizable(True,False)
-        
-    def addInterfaces(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-        
-        self.hostLabel = tkinter.Label(text="Host")
-        self.hostLabel.grid(column=0,row=0,sticky="EW",padx=5,pady=5)
-        
-        self.hostEntry = tkinter.Entry()
-        self.hostEntry.grid(column=1,row=0,sticky="EW",padx=5,pady=5)
-        self.hostEntry.bind("<Return>",self.input(interface))
-        interface.append = self.hostEntry.get()
-        
-        self.nameLabel = tkinter.Label(text="Name")
-        self.nameLabel.grid(column=0,row=1,sticky="EW",padx=5,pady=5)
-        
-        self.nameEntry = tkinter.Entry()
-        self.nameEntry.grid(column=1,row=1,sticky="EW",padx=5,pady=5)
-        self.nameEntry.bind("<Return>",self.input(interface))
-        interface.append = self.nameEntry.get()
-        
-        self.configLabel = tkinter.Label(text="Config")
-        self.configLabel.grid(column=0,row=2,sticky="EW",padx=5,pady=5)
-        
-        self.configEntry = tkinter.Entry()
-        self.configEntry.grid(column=1,row=2,sticky="EW",padx=5,pady=5)
-        self.configEntry.bind("<Return>",self.input(interface))
-        interface.append = self.configEntry.get()
-        
-        self.vFourLabel = tkinter.Label(text="IPv4")
-        self.vFourLabel.grid(column=0,row=3,sticky="EW",padx=5,pady=5)
-        
-        self.vFourEntry = tkinter.Entry()
-        self.vFourEntry.grid(column=1,row=3,sticky="EW",padx=5,pady=5)
-        self.vFourEntry.bind("<Return>",self.input(interface))
-        interface.append = self.vFourEntry.get()
-        
-        self.vSixLabel = tkinter.Label(text="IPv6")
-        self.vSixLabel.grid(column=0,row=4,sticky="EW",padx=5,pady=5)
-        
-        self.vSixEntry = tkinter.Entry()
-        self.vSixEntry.grid(column=1,row=4,sticky="EW",padx=5,pady=5)
-        self.vSixEntry.bind("<Return>",self.input(interface))
-        interface.append = self.vSixEntry.get()
-        
-        self.broadcastLabel = tkinter.Label(text="Broadcast")
-        self.broadcastLabel.grid(column=0,row=5,sticky="EW",padx=5,pady=5)
-        
-        self.broadcastEntry = tkinter.Entry()
-        self.broadcastEntry.grid(column=1,row=5,sticky="EW",padx=5,pady=5)
-        self.broadcastEntry.bind("<Return>",self.input(interface))
-        interface.append = self.broadcastEntry.get()
-        
-        self.gatewayLabel = tkinter.Label(text="Gateway")
-        self.gatewayLabel.grid(column=0,row=6,sticky="EW",padx=5,pady=5)
-        
-        self.gatewayEntry = tkinter.Entry()
-        self.gatewayEntry.grid(column=1,row=6,sticky="EW",padx=5,pady=5)
-        self.gatewayEntry.bind("<Return>",self.input(interface))
-        interface.append = self.gatewayEntry.get()
-        
-        self.netLabel = tkinter.Label(text="Network")
-        self.netLabel.grid(column=0,row=7,sticky="EW",padx=5,pady=5)
-        
-        self.netEntry = tkinter.Entry()
-        self.netEntry.grid(column=1,row=7,sticky="EW",padx=5,pady=5)
-        self.netEntry.bind("<Return>",self.input(interface))
-        interface.append = self.netEntry.get()
-        
-        self.nextPage = tkinter.Button(text="Next Page",command=self.addContent)
-        self.nextPage.grid(column=0,row=8,sticky="EW",padx=5,pady=5,columnspan=2)
-        
-    def input(self,inputs):
-        print(inputs)
-    
-    def addContent(self):
-        print("it works")
+    def addEntry(self,label,parent):
+        self.entries.append(label)
+        updateDisplayFrame(parent,self.entries)
+
+class Host:
+    def __init__(self):
+        pass
+
+def updateDisplayFrame(frame,data):
+    row = 0
+    for entry in data:
+        if (isinstance(entry,DNSEntry)):
+            name = tkinter.Label(frame,text=entry.name)
+            name.grid(column=0,row=row,sticky="EW",padx=5,pady=5)
+            type = tkinter.Label(frame,text=entry.type)
+            type.grid(column=1,row=row,sticky="EW",padx=5,pady=5)
+            value = tkinter.Label(frame,text=entry.value)
+            value.grid(column=2,row=row,sticky="EW",padx=5,pady=5)
+        else: 
+            label = tkinter.Label(frame,text=entry)
+            label.grid(column=0,row=row,sticky="EW",padx=5,pady=5)
+        row += 1
 
 if __name__ == "__main__":
-    interface = interface(None)
-    interface.title("OCCP Scenario Builder")
-    interface.mainloop()
+    root = tkinter.Tk()
+    note = tkinter.ttk.Notebook(root)
     
-# def addContent():
-    # clear(mainframe)
+    root.title("OCCP Scenario Builder")
     
-    # tkinter.tix.LabelEntry()
-    # tkinter.Label(mainframe,text="adding content")
-    # tkinter.Button(mainframe,text="Next Page",command=addTeam)
+    networkTab = tkinter.Frame(note)
+    hostsTab = tkinter.Frame(note)
     
-# def addTeam():
-    # clear(mainframe)
-    # tkinter.Label(mainframe,text="adding teams") 
-    # tkinter.Button(mainframe,text="Next Page",command=addTeamEvent) 
+    dnsFrame = tkinter.Frame(networkTab)
+    dnsFrame.grid()
+    dnsFrame.grid(column=0,row=0)
+    netFrame = tkinter.Frame(networkTab)
+    netFrame.grid()
+    netFrame.grid(column=0,row=1)
     
-# def addTeamEvent():
-    # clear(mainframe)
-    # tkinter.Label(mainframe,text="adding team events") 
-    # tkinter.Button(mainframe,text="Next Page",command=addEventParams) 
+    dnsTitle = tkinter.Label(dnsFrame,text="DNS Records")
+    dnsTitle.grid(column=0,row=0,sticky="EW",padx=5,pady=5,columnspan=4)
     
-# def addEventParams():
-    # clear(mainframe)
-    # tkinter.Label(mainframe,text="adding event parameters") 
-    # tkinter.Button(mainframe,text="Next Page") 
+    nameLabel = tkinter.Label(dnsFrame,text="Name")
+    nameLabel.grid(column=0,row=1,sticky="EW",padx=5,pady=5)
+    typeLabel = tkinter.Label(dnsFrame,text="Type")
+    typeLabel.grid(column=1,row=1,sticky="EW",padx=5,pady=5)
+    valueLabel = tkinter.Label(dnsFrame,text="Value")
+    valueLabel.grid(column=2,row=1,sticky="EW",padx=5,pady=5)
     
-# if __name__ == '__main__':
-    # root = tkinter.tix.Tk()
-    # root.title("OCCP Scenario Builder")
-    # mainframe = Frame(root)
-    # mainframe.grid()
+    rootDNS = RootDNS()
+    recordsFrame = tkinter.Frame(dnsFrame)
+    recordsFrame.grid()
+    recordsFrame.grid(column=0,row=3,columnspan=4)
     
-    # #scen = ScenarioGenerator.ScenarioGen()
+    nameEntry = tkinter.Entry(dnsFrame)
+    nameEntry.grid(column=0,row=2,sticky="EW",padx=5,pady=5)
+    typeEntry = tkinter.Entry(dnsFrame)
+    typeEntry.grid(column=1,row=2,sticky="EW",padx=5,pady=5)
+    valueEntry = tkinter.Entry(dnsFrame)
+    valueEntry.grid(column=2,row=2,sticky="EW",padx=5,pady=5)
+    addButton = tkinter.Button(dnsFrame,text="Add",command= lambda:rootDNS.addEntry(nameEntry.get(),typeEntry.get(),valueEntry.get(),recordsFrame))
+    addButton.grid(column=3,row=2,sticky="EW",padx=5,pady=5)
+    updateDisplayFrame(recordsFrame,rootDNS.entries)
     
-    # tkinter.Button(mainframe,text="Begin",command=addInterfaces).grid(row=0,column=0,sticky="e",columnspan=2)
-        
-    # root.mainloop()
+    entryFrame = tkinter.Frame(dnsFrame)
+    
+    netTitle = tkinter.Label(netFrame,text="Network Labels")
+    netTitle.grid(column=0,row=0,sticky="EW",padx=5,pady=5,columnspan=3)
+    
+    net = Network()
+    labelsFrame = tkinter.Frame(netFrame)
+    labelsFrame.grid()
+    labelsFrame.grid(column=0,row=2,columnspan=3)
+    
+    label = tkinter.Label(netFrame,text="Label")
+    label.grid(column=0,row=1,sticky="EW",padx=5,pady=5)
+    entry = tkinter.Entry(netFrame)
+    entry.grid(column=1,row=1,sticky="EW",padx=5,pady=5)
+    addButton = tkinter.Button(netFrame,text="Add",command = lambda:net.addEntry(entry.get(),labelsFrame))
+    addButton.grid(column=2,row=1,sticky="EW",padx=5,pady=5)
+    updateDisplayFrame(labelsFrame,net.entries)
+    
+    note.add(networkTab,text="Network")
+    note.add(hostsTab,text="Hosts")
+    
+    note.grid()
+    
+    root.mainloop()
+    exit()
